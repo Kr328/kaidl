f17b230be235894c2232d7e40565a6246339fdcb
Kr328 <kr328app@outlook.com>
2020-12-05 12:55:42 +0800

    Chore: add kotlin/java target jvm version

--- kaidl-runtime/build.gradle.kts
+++ kaidl-runtime/build.gradle.kts
@@ -1,5 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("kotlin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+tasks.withType(KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = "1.7"
+    }
 }
 
 dependencies {
--- kaidl/build.gradle.kts
+++ kaidl/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("application")
     id("kotlin")
@@ -10,6 +12,12 @@ val gKotlinpoetVersion: String by rootProject.extra
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType(KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
